da60336d93bb3393b9391519fe18f264b9c61021
Gonzalo Berdugo <Gonzalober16@gmail.com>
2026-09-22 04:08:23 -0300

    Habilito Swagger fuera de modo Development
    Saco la condicion de Development para que Swagger este disponible en Render

--- MiApiTP/Program.cs
+++ MiApiTP/Program.cs
@@ -89,13 +89,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapOpenApi();
 
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
